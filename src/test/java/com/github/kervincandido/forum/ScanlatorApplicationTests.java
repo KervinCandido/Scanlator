1ebca903fed4f9957c802b9c1d3984970801e796
KervinCandido <kervincandido@gmail.com>
2020-11-25 19:46:24 -0300

    primeiro commit

---
+++ src/test/java/com/github/kervincandido/forum/ScanlatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kervincandido.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanlatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
